7b33f834a03b155ebce0e44022b93b060bb7d7b9
benswift404 <swiftmac234@gmail.com>
2015-10-06 16:28:56 -0400

    first commit
    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CrudProj::Application.config.secret_key_base = '6d52b02132e01b4cc191a77ad75cba1714318b32552cbe9512dae8a8f70726498ea3a8638a0875d2c52be39e4512eee2da1f48929cda7a29f86936683e475d59'
